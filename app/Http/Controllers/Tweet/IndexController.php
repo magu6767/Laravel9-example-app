c8636c6d8140cfcd17cf03ee19a3f8fd2178e1bd
Hideto Maguchi <hidetomaguchi@gmail.com>
2024-12-10 08:53:10 +0900

    チャプター3-1

--- app/Http/Controllers/Tweet/IndexController.php
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -3,17 +3,17 @@
 namespace App\Http\Controllers\Tweet;
 
 use App\Http\Controllers\Controller;
-use App\Models\Tweet;
 use Illuminate\Http\Request;
+use App\Services\TweetService;
 
 class IndexController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function __invoke(Request $request)
+    public function __invoke(Request $request, TweetService $tweetService)
     {
-        $tweets = Tweet::orderBy('created_at', 'DESC')->get();
+        $tweets = $tweetService->getTweets();
         return view('tweet.index', ['tweets' => $tweets]);
     }
 }
